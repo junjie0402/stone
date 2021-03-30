91b3a90fadb0bef9d925640d78b71a12824cb25b
junexian <junexian@tencent.com>
2021-03-30 14:21:33 +0800

    第三章：分割单词，添加学习注释

--- src/chap3/LexerRunner.java
+++ src/chap3/LexerRunner.java
@@ -1,6 +1,7 @@
 package chap3;
 import stone.*;
 
+//整个源代码其实就是一个很长很长的字符串。语言处理器首先会进行词法分析。
 public class LexerRunner {
     public static void main(String[] args) throws ParseException {
         Lexer l = new Lexer(new CodeDialog());
